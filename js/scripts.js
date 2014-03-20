fbc7ef98e667e2b4785a096f597b5c057fd614f4
Martin Adamko <martin@attitude.sk>
2014-03-20 18:16:34 +0100

    Major update with revamped demo
    * more options to overwrite (timeout, breakpoints,...)
    * module divided for vertical and horizontal rhythm
    * new optional experience `since` (since phone, since tablet)
    * current experience (either phone, tablet or desktop,...)
    * maximum control of class `media-experience` prefix and `since`

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,29 @@ var mediaExperience = function(options, breakpoints) {
         target: null,
         regex:  null,
         options: {
-            useBreakpoints: true,
-            module: 80,
-            modules: 12,
+            horizontalModule: 80,
+            verticalModule: 80,
+            prefix: "media-experience",
             target: "body",
-            prefix: "media"
-        },
-        // Columns to be true
-        breakpoints: {
-            'phone':               0, // mobile first
-              'phone--small':      4, // 80 x   0...4 =    0 <= ? < 320
-              'phone--medium':     6, // 80 x   5...6 =  320 <= ? < 480
-              'phone--large':      9, // 80 x   7...9 =  480 <= ? < 720
-            'tablet':               9, // 80 x  9 =  720
-              'tablet--small':     10, // 80 x  9...10 =  720 <= ? <  800
-              'tablet--medium':    11, // 80 x  9...10 =  720 <= ? <  800
-              'tablet--large':     13, // 80 x 11...12 =  800 <= ? <  960
-            'desktop':             13, // 80 x 13 =  960
-              'desktop--small':    14, // 80 x 13...14 =  960 <= ? < 1120
-              'desktop--medium':   18, // 80 x 15...18 = 1120 <= ? < 1440
-              'desktop--large':    24, // 80 x 17...24 = 1440 <= ? < 1920
-            'oversized-desktop':   24  // 80 x 24 = 1920
+            timeout: 300,
+            useBreakpoints: true,
+            useSince: 'since',
+            // Columns to be true
+            breakpoints: {
+                'phone':               0, // mobile first
+                  'phone--small':      4, // 80 x   0...4 =    0 <= ? < 320
+                  'phone--medium':     6, // 80 x   5...6 =  320 <= ? < 480
+                  'phone--large':      9, // 80 x   7...9 =  480 <= ? < 720
+                'tablet':               9, // 80 x  9 =  720
+                  'tablet--small':     10, // 80 x  9...10 =  720 <= ? <  800
+                  'tablet--medium':    11, // 80 x  9...10 =  720 <= ? <  800
+                  'tablet--large':     13, // 80 x 11...12 =  800 <= ? <  960
+                'desktop':             13, // 80 x 13 =  960
+                  'desktop--small':    14, // 80 x 13...14 =  960 <= ? < 1120
+                  'desktop--medium':   18, // 80 x 15...18 = 1120 <= ? < 1440
+                  'desktop--large':    24, // 80 x 17...24 = 1440 <= ? < 1920
+                'oversized-desktop':   24  // 80 x 24 = 1920
+            }
         },
         init: function() {
             var self = this;
@@ -39,8 +41,8 @@ var mediaExperience = function(options, breakpoints) {
         resize: function() {
             var self = this;
 
-            var widths  = parseInt($(self.target).width() / self.options.module);
-            var heights = parseInt($(self.target).height() / self.options.module);
+            var widths  = parseInt($(self.target).width() / self.options.horizontalModule);
+            var heights = parseInt($(self.target).height() / self.options.verticalModule);
 
             var classes = [];
 
@@ -49,49 +51,45 @@ var mediaExperience = function(options, breakpoints) {
 
             if (self.options.useBreakpoints) {
                 var lastBreakpoint = 0;
+                var lastMajorBreakpoint = 0;
 
-                for (var attrname in self.breakpoints) {
+                for (var attrname in self.options.breakpoints) {
+                    // console.log('lastMajorBreakpoint', lastMajorBreakpoint, 'lastBreakpoint', lastBreakpoint, 'widths', widths, attrname, self.options.breakpoints[attrname])
 
-                        if (attrname.match(/--/)) {
-                            // console.log('lastBreakpoint', lastBreakpoint, 'widths', widths, attrname, self.breakpoints[attrname])
-                            if (lastBreakpoint <= widths && widths < self.breakpoints[attrname]) {
-                                classes.push(self.options.prefix+'-experience-'+attrname);
-                            }
-                        } else if (self.breakpoints[attrname] <= widths) {
-                            classes.push(self.options.prefix+'-experience-'+attrname);
+                    if (attrname.match(/--/)) {
+                        // Variations of the major breakpoint
+                        if (lastBreakpoint <= widths && widths < self.options.breakpoints[attrname]) {
+                            classes.unshift(self.options.prefix+'-'+attrname);
+                        }
+                    } else if (self.options.breakpoints[attrname] <= widths) {
+                        // Major breakpoints
+                        if (self.options.useSince && typeof self.options.useSince === 'string') {
+                            // All major breakpoints: mobile first approach
+                            classes.push(self.options.prefix+'-'+self.options.useSince+'-'+attrname);
                         }
 
-                    lastBreakpoint = self.breakpoints[attrname];
+                        if (self.options.breakpoints[attrname] <= widths) {
+                            // Only current major breakpoint (remember it)
+                            lastMajorBreakpoint = attrname;
+                        }
+                    }
+
+                    lastBreakpoint = self.options.breakpoints[attrname];
                 }
             }
 
-            // Min widths
-            // for(var i=1; (i <= widths && i < self.options.modules); i++) {
-            //     classes.push(self.options.prefix+'-min-width-'+i);
-            // }
+            // Add current major breakpoint
+            classes.unshift(self.options.prefix+'-'+lastMajorBreakpoint);
 
-            // Max widths
-            // for(var i=widths+1; i <= self.options.modules; i++ ) {
-            //     classes.push(self.options.prefix+'-max-width-'+i);
-            // }
-
-            // Min heights
-            // for(var i=1; (i <= heights && i < self.options.modules); i++) {
-            //     classes.push(self.options.prefix+'-min-height-'+i);
-            // }
-
-            // Max heights
-            // for(var i=heights+1; i <= self.options.modules; i++ ) {
-            //     classes.push(self.options.prefix+'-max-height-'+i);
-            // }
-
+            // Build the classes
             classes = classes.join(' ');
 
+            // Remove old prefix-matching classes and add new
             self.target.removeClass(function (index, css) {
                 return (css.match(self.regex) || []).join(' ');
             }).addClass(classes);
 
-            // console.log(widths, heights, self.options.module);
+            // console.log(widths, heights, self.options.horizontalModule, self.options.verticalModule);
 
             console.log('resize();');
         }
@@ -107,7 +105,7 @@ var mediaExperience = function(options, breakpoints) {
     // Merge passed breakpoints
     if (typeof breakpoints === 'object') {
         for (var attrname in breakpoints) {
-            self.breakpoints[attrname] = breakpoints[attrname];
+            self.options.breakpoints[attrname] = breakpoints[attrname];
         }
     };
 
@@ -120,10 +118,10 @@ var mediaExperience = function(options, breakpoints) {
 
     window.onresize = function(){
         clearTimeout(trigger);
-        trigger = setTimeout(triggerResize, 300);
+        trigger = setTimeout(triggerResize, self.options.timeout);
     };
 
     return self;
 };
 
-var bodyWatcher = mediaExperience({useBreakpoints: true, prefix: "media", module: 80, modules: 16});
+var bodyWatcher = mediaExperience({useBreakpoints: true, prefix: "media-experience", timeout: 200});
