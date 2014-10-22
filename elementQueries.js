56372d9c0c9fc25ac54015536eb6e330ac449bf3
Martin Adamko <martin@attitude.sk>
2014-10-22 15:30:03 +0200

    Breaking change: Change project name
    Unifies classes

--- elementQueries.js
+++ elementQueries.js
@@ -1,6 +1,6 @@
 /*jslint browser: true*/
 /*!
- * mediaexperienceJS v0.3.2
+ * ElementQueriesJS v0.3.3
  *
  * Copyright 2014 Martin Adamko
  * Released under the MIT license
@@ -12,34 +12,34 @@
      * @constructor
      */
     var console = window.console,
-        MediaExperience = function (elem, options) {
+        ElementQueries = function (elem, options) {
             this.elem = elem;
             this.$elem = $(elem);
             this.options = options;
 
             // Distinc
-            this.$elem.addClass('responds-to-media-experience');
+            this.$elem.addClass('is-responsive');
 
             // Customization of the plugin on a per-element basis.
-            // <div data-mediaexperience-options='{prefix: "device"}'></div>
-            this.metadata = this.$elem.data('mediaexperience-options');
+            // <div data-eq='{prefix: "device"}'></div>
+            this.metadata = this.$elem.data('eq');
         },
-        // Array of all media experience instances
-        allMediaExperienceElements = [],
+        // Array of all instances
+        allElementQueriesElements = [],
         // timeout for onresize events
         windowResizeTimeout,
 
         // Trigger Custom Event
         // Shim:
-        shimTriggerMediaExperienceChanged = function shimTriggerMediaExperienceChanged() {
+        shimTriggerEQChanged = function shimTriggerEQChanged() {
             var e = document.createEvent('Event');
 
-            e.initEvent(MediaExperience.prototype.eventName, true, false);
+            e.initEvent(ElementQueries.prototype.eventName, true, false);
             window.dispatchEvent(e);
         },
         // Modern browsers: CustomEvent
-        triggerMediaExperienceChanged = function triggerMediaExperienceChanged() {
-            var e = new window.CustomEvent(MediaExperience.prototype.eventName, {
+        triggerEQChanged = function triggerEQChanged() {
+            var e = new window.CustomEvent(ElementQueries.prototype.eventName, {
                 'bubbles': true
             });
 
@@ -52,61 +52,61 @@
                 l, // length
                 n, // length
                 // groups of active instances by number of parents
-                mediaExperienceParents = {
+                elementQueriesParents = {
                     'counts': [],
                     'parentsCountOf': []
                 },
                 parentsCount;
 
-            l = allMediaExperienceElements.length;
+            l = allElementQueriesElements.length;
 
             for (i = 0; i < l; i += 1) {
-                parentsCount = allMediaExperienceElements[i].$elem.parents('.responds-to-media-experience').length;
+                parentsCount = allElementQueriesElements[i].$elem.parents('.is-responsive').length;
 
                 // Create empty array if not yet exists...
-                if (mediaExperienceParents.parentsCountOf[parentsCount] === undefined) {
-                    mediaExperienceParents.parentsCountOf[parentsCount] = [];
+                if (elementQueriesParents.parentsCountOf[parentsCount] === undefined) {
+                    elementQueriesParents.parentsCountOf[parentsCount] = [];
                 }
                 // ... and rememter the element's index
-                mediaExperienceParents.parentsCountOf[parentsCount].push(i);
+                elementQueriesParents.parentsCountOf[parentsCount].push(i);
 
                 // Remember what possible counts we encountered
-                if (mediaExperienceParents.counts.indexOf(parentsCount) === -1) {
-                    mediaExperienceParents.counts.push(parentsCount);
+                if (elementQueriesParents.counts.indexOf(parentsCount) === -1) {
+                    elementQueriesParents.counts.push(parentsCount);
                 }
             }
 
             // Sort number of parents
-            mediaExperienceParents.counts.sort();
+            elementQueriesParents.counts.sort();
 
-            // console.log(mediaExperienceParents);
+            // console.log(elementQueriesParents);
 
-            l = mediaExperienceParents.counts.length;
+            l = elementQueriesParents.counts.length;
 
             for (i = 0; i < l; i += 1) {
-                parentsCount = mediaExperienceParents.counts[i];
+                parentsCount = elementQueriesParents.counts[i];
 
                 // console.log('parentsCountOf:' + parentsCount);
 
-                n = mediaExperienceParents.parentsCountOf[parentsCount].length;
+                n = elementQueriesParents.parentsCountOf[parentsCount].length;
 
                 for (j = 0; j < n; j += 1) {
                     // console.log('iteration: ' + j);
-                    allMediaExperienceElements[mediaExperienceParents.parentsCountOf[parentsCount][j]].resize();
+                    allElementQueriesElements[elementQueriesParents.parentsCountOf[parentsCount][j]].resize();
                 }
             }
 
-            MediaExperience.triggerEvent();
+            ElementQueries.triggerEvent();
         };
 
     // the plugin prototype
-    MediaExperience.prototype = {
+    ElementQueries.prototype = {
         timeout: 200,
-        eventName: 'mediaexperiencechanged',
+        eventName: 'eqchanged',
         defaults: {
             horizontalModule: 80,
             verticalModule: 80,
-            prefix: 'media-experience',
+            prefix: 'eq-',
             useBreakpoints: true,
             calculateVertical: false,
             useSince: 'since',
@@ -114,17 +114,17 @@
             breakpoints: {
                 'watch': 0, // too tiny
                 'phone': 4, // mobile first
-                'phone--small': 4, // 80 x   0...4 =    0 <= ? < 320
-                'phone--medium': 6, // 80 x   5...6 =  320 <= ? < 480
-                'phone--large': 9, // 80 x   7...9 =  480 <= ? < 720
+                'phone.small': 4, // 80 x   0...4 =    0 <= ? < 320
+                'phone.medium': 6, // 80 x   5...6 =  320 <= ? < 480
+                'phone.large': 9, // 80 x   7...9 =  480 <= ? < 720
                 'tablet': 9, // 80 x  9 =  720
-                'tablet--small': 10, // 80 x  9...10 =  720 <= ? <  800
-                'tablet--medium': 11, // 80 x  9...10 =  720 <= ? <  800
-                'tablet--large': 13, // 80 x 11...12 =  800 <= ? <  960
+                'tablet.small': 10, // 80 x  9...10 =  720 <= ? <  800
+                'tablet.medium': 11, // 80 x  9...10 =  720 <= ? <  800
+                'tablet.large': 13, // 80 x 11...12 =  800 <= ? <  960
                 'desktop': 13, // 80 x 13 =  960
-                'desktop--small': 14, // 80 x 13...14 =  960 <= ? < 1120
-                'desktop--medium': 18, // 80 x 15...18 = 1120 <= ? < 1440
-                'desktop--large': 24, // 80 x 17...24 = 1440 <= ? < 1920
+                'desktop.small': 14, // 80 x 13...14 =  960 <= ? < 1120
+                'desktop.medium': 18, // 80 x 15...18 = 1120 <= ? < 1440
+                'desktop.large': 24, // 80 x 17...24 = 1440 <= ? < 1920
                 'oversized-desktop': 24 // 80 x 24 = 1920
             }
         },
@@ -134,10 +134,10 @@
             // globally or using an object literal.
             this.config = $.extend({}, this.defaults, this.options, this.metadata);
 
-            this.regex = new RegExp('\\b' + this.config.prefix + '-\\S+', 'g');
+            this.regex = new RegExp('\\b' + this.config.prefix + '(?:width|height|is-horizontal|is-vertical)-?\\S*', 'g');
             this.resize();
 
-            MediaExperience.triggerEvent();
+            ElementQueries.triggerEvent();
 
             return this;
         },
@@ -154,7 +154,7 @@
                 lastMajorHeightBreakpoint,
                 attrname;
 
-            if ($(self.$elem).hasClass('media-experience-disabled')) {
+            if ($(self.$elem).hasClass('eq-off')) {
                 return;
             }
 
@@ -163,7 +163,7 @@
 
             width = $(self.$elem).width();
             widths = parseInt(width / self.config.horizontalModule, 10);
-            classes.push(self.config.prefix + '-width-' + widths);
+            classes.push(self.config.prefix + 'width-' + widths);
 
             if (!!self.config.calculateVertical) {
                 console.log(self.$elem[0].nodeName.toLowerCase());
@@ -173,7 +173,7 @@
                     height = $(self.$elem).height();
                 }
                 heights = parseInt(height / self.config.verticalModule, 10);
-                classes.push(self.config.prefix + '-height-' + heights);
+                classes.push(self.config.prefix + 'height-' + heights);
             }
 
             if (self.config.useBreakpoints) {
@@ -183,14 +183,14 @@
                     if (self.config.breakpoints.hasOwnProperty(attrname)) {
                         // console.log('lastMajorBreakpoint', lastMajorBreakpoint, 'lastBreakpoint', lastBreakpoint, 'widths', widths, attrname, self.config.breakpoints[attrname]);
                         // Variations of the major breakpoint
-                        if (attrname.match(/--/)) {
+                        if (attrname.match(/\./)) {
                             if (lastBreakpoint <= widths && widths < self.config.breakpoints[attrname]) {
-                                classes.push(self.config.prefix + '-' + attrname);
+                                classes.push(self.config.prefix + 'width-' + attrname.replace(/\./, '-'));
                             }
 
                             if (!!self.config.calculateVertical) {
                                 if (lastBreakpoint <= heights && heights < self.config.breakpoints[attrname]) {
-                                    classes.push(self.config.prefix + '-' + attrname + '-height');
+                                    classes.push(self.config.prefix + 'height-' + attrname.replace(/\./, '-'));
                                 }
                             }
                         } else {
@@ -198,7 +198,7 @@
                                 // Major breakpoints
                                 if (self.config.useSince && typeof self.config.useSince === 'string') {
                                     // All major breakpoints: mobile first approach
-                                    classes.push(self.config.prefix + '-' + self.config.useSince + '-' + attrname);
+                                    classes.push(self.config.prefix + 'width-' + self.config.useSince + '-' + attrname);
                                 }
 
                                 if (self.config.breakpoints[attrname] <= widths) {
@@ -211,7 +211,7 @@
                                 // Major breakpoints
                                 if (self.config.useSince && typeof self.config.useSince === 'string') {
                                     // All major breakpoints: mobile first approach
-                                    classes.push(self.config.prefix + '-' + self.config.useSince + '-' + attrname + '-height');
+                                    classes.push(self.config.prefix + 'height-' + self.config.useSince + '-' + attrname);
                                 }
 
                                 if (self.config.breakpoints[attrname] <= heights) {
@@ -227,13 +227,13 @@
             }
 
             // Add current major breakpoint
-            classes.push(self.config.prefix + '-' + lastMajorBreakpoint);
+            classes.push(self.config.prefix + 'width-' + lastMajorBreakpoint);
 
             if (self.config.calculateVertical) {
                 // Add current major vertical breakpoint
-                classes.push(self.config.prefix + '-' + lastMajorHeightBreakpoint + '-height');
+                classes.push(self.config.prefix + 'height-' + lastMajorHeightBreakpoint);
                 // Orientation
-                classes.push(self.config.prefix + '-orientation-' + (width > height ? 'horizontal' : 'vertical'));
+                classes.push(self.config.prefix + (width > height ? 'is-horizontal' : 'is-vertical'));
             }
 
             // Build the classes
@@ -249,37 +249,37 @@
         }
     };
 
-    MediaExperience.triggerEvent = function () {
+    ElementQueries.triggerEvent = function () {
         if (window.CustomEvent) {
-            triggerMediaExperienceChanged();
+            triggerEQChanged();
         } else {
             // Fallback
-            shimTriggerMediaExperienceChanged();
+            shimTriggerEQChanged();
         }
 
         return;
     };
 
-    MediaExperience.defaults = MediaExperience.prototype.defaults;
+    ElementQueries.defaults = ElementQueries.prototype.defaults;
 
     // Fire only after all window.on('resize') events finished.
     $(window).on('resize', function runOnWindowResize() {
         // Remove previous timer if still within previous timeout
         window.clearTimeout(windowResizeTimeout);
         // Set new timeout to call function
-        windowResizeTimeout = window.setTimeout(triggerResize, MediaExperience.prototype.timeout);
+        windowResizeTimeout = window.setTimeout(triggerResize, ElementQueries.prototype.timeout);
     });
 
-    $.fn.mediaExperience = function newMediaExperience(options) {
+    $.fn.elementQueries = function newElementQueries(options) {
         this.each(function () {
-            var v = new MediaExperience(this, options).init();
+            var v = new ElementQueries(this, options).init();
 
-            allMediaExperienceElements.push(v);
+            allElementQueriesElements.push(v);
         });
 
         return this;
     };
 
     // Allows overwriting of any sitewide plugin aspect like timeout
-    window.MediaExperience = MediaExperience;
+    window.ElementQueries = ElementQueries;
 }(window.jQuery, window, document));
